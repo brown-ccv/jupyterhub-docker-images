cae9409bbcb1784e4e816fae0b3e913ab9eab739
Paul Stey <paulstey@gmail>
2021-08-06 16:58:52 -0400

    add php2560 course

---
+++ requirements/classes/php2560/packages.R
@@ -0,0 +1,9 @@
+# Use this script to install packages via CRAN, for example: 
+
+# NOTE: Code below shows how you install R packages from CRAN and Bioconductor. For CRAN packages, you can use 
+# the standard install.packages() function; for Bioconductor packages, however, you 
+# must first install BiocManager and then use that for installs. 
+
+install.packages(c("tidyverse"), 
+                 dependencies = TRUE, 
+                 repos = 'http://cran.rstudio.com/')
