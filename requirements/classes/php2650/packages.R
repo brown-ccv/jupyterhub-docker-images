498a058d4db2307036e9102b9866d689c98d91dc
prasad.bandarkar <prasad_bandarkar@brown.edu>
2022-01-18 16:56:24 -0500

    packages.R and requirements.jl modified

---
+++ requirements/classes/php2650/packages.R
@@ -0,0 +1,16 @@
+# Use this script to install packages via CRAN, for example: 
+
+# NOTE: Code below shows how you install R packages from CRAN and Bioconductor. For CRAN packages, you can use 
+# the standard install.packages() function; for Bioconductor packages, however, you 
+# must first install BiocManager and then use that for installs. 
+
+# install.packages("BiocManager", dependencies=TRUE, repos='http://cran.rstudio.com/')
+# install.packages(c("ggplot2", "pheatmap", "RColorBrewer", "PoiClaClu",
+#                    "patchwork", "tidyr", "GGally"), dependencies=TRUE, 
+#                  repos='http://cran.rstudio.com/')
+# BiocManager::install(c("airway", "DESeq2", "vsn", "biomaRt",
+#                        "AnnotationHub", "SummarizedExperiment"))
+install.packages(c( "tidyverse", "nycflights13", "parallel", "microbenchmark", "foreach",
+                   "doParallel", "kernlab", "ISLR", "MASS", "e1071", "pROC", "tree",
+                   "rpart", "caret", "doMC", "randomForest", "gbm", "Metrics", "keras", "tfdatasets") ,
+                   dependencies=TRUE, repos='http://cran.rstudio.com')
