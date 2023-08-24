0f454a95786a7644d225e85a8971097f473cdc99
Prasad Bandarkar <bandarkar.prasad@gmail.com>
2023-08-24 12:22:57 -0400

    mpa2065 same as basepackages

--- requirements/classes/mpa2065/requirements.jl
+++ requirements/classes/mpa2065/requirements.jl
@@ -1,11 +1,5 @@
 const julia_packages = [
-                        "IJulia", # DO NOT REMOVE
-                        # add class specific classes here:
-                        "CSV",
-                        "DataFrames",
-                        "DecisionTree",
-                        "Primes",
-                        "StatsBase",
-                        "MLJ",
-                        "Plots"
-                        ]
+                    "IJulia", # DO NOT REMOVE
+                   # add class specific classes here:
+
+                    ]
