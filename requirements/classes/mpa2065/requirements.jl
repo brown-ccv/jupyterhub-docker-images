f40f1c9d7b2b2405fe2db23463123bde647ca40c
Maria Isabel Restrepo <maria_restrepo@brown.edu>
2024-08-01 15:53:03 -0400

    temp(mpa2065): comment out requirements for faster ci

--- requirements/classes/mpa2065/requirements.jl
+++ requirements/classes/mpa2065/requirements.jl
@@ -1,12 +1,3 @@
 const julia_packages = [
                     "IJulia", # DO NOT REMOVE
-                   # add class specific classes here:
-                   "CSV",
-                   "DataFrames",
-                   "DecisionTree",
-                   "Primes",
-                   "StatsBase",
-                   "MLJ",
-                   "Plots",
-                   "Grisu"
                     ]
