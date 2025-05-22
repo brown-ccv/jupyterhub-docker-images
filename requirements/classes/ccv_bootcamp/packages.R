e03da3d17d8986ba1d3f6c1e7ea7363e15244bb5
Joselynn Wallace <joselynn_wallace@brown.edu>
2025-05-22 11:27:08 -0400

    Update packages.R

--- requirements/classes/ccv_bootcamp/packages.R
+++ requirements/classes/ccv_bootcamp/packages.R
@@ -4,6 +4,6 @@
 # the standard install.packages() function; for Bioconductor packages, however, you 
 # must first install BiocManager and then use that for installs. 
 
-install.packages(c("tidyverse", ("BiocManager"), dependencies=TRUE, repos='http://cran.rstudio.com/')
+install.packages(c("tidyverse", "BiocManager"), dependencies=TRUE, repos='http://cran.rstudio.com/')
 
 install.packages(c("GGally", "ggrepel", "kableExtra", "knitr", "maps", "openxlsx", "patchwork", "pdftools", "pheatmap", "pillar", "plotly", "R.utils", "R2HTML", "RColorBrewer", "remotes", "rgeos", "spatstat.explore", "spatstat.geom", "stringr", "viridis", "tesseract","zoo", "gridExtra"), repos = "http://cran.rstudio.com/", dependencies = TRUE)
